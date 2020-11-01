994440b89357711f266f246abd99192f559b9208
t-kurihara <takuya.kurihara00@gmail.com>
2020-11-01 21:24:11 +0900

    Send message to SQS from lambda.

--- lib/health_logger-stack.ts
+++ lib/health_logger-stack.ts
@@ -2,13 +2,26 @@ import {resolve} from "path";
 import * as cdk from '@aws-cdk/core';
 import { NodejsFunction } from "@aws-cdk/aws-lambda-nodejs";
 import { LambdaRestApi } from "@aws-cdk/aws-apigateway";
+import { Queue } from "@aws-cdk/aws-sqs";
+import { PolicyStatement } from "@aws-cdk/aws-iam";
 
 export class HealthLoggerStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    const queue = new Queue(this, "FileCreateQueue");
+
     const lambda = new NodejsFunction(this, 'LambdaHandler', {
       entry: resolve(__dirname, './lambda/FileUploadEventHandler.ts'),
+      environment: {
+        QUEUE_URL: queue.queueUrl,
+      },
+      initialPolicy: [
+        new PolicyStatement({
+          actions: ["sqs:SendMessage"],
+          resources: [queue.queueArn],
+        })
+      ],
     });
 
     const api = new LambdaRestApi(this, 'FileUploadEventAPI', {
--- lib/lambda/FileUploadEventHandler.ts
+++ lib/lambda/FileUploadEventHandler.ts
@@ -1,7 +1,39 @@
 import { APIGatewayProxyHandlerV2 } from "aws-lambda";
+import { SQS } from "aws-sdk";
 
-export const handler: APIGatewayProxyHandlerV2 = async (event, context, callback) => {
+export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   console.log(JSON.stringify(event.body, null, 2));
+
+  if (event.body == null) {
+    console.error("Request has no body.")
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(event.body),
+    };
+  }
+
+  const slackEvent = JSON.parse(event.body);
+  const messageBody = JSON.stringify({
+    fileId: slackEvent?.event?.file_id,
+  })
+
+  const queueUrl = process.env.QUEUE_URL ?? "";
+
+  const sqs = new SQS();
+
+  try {
+    await sqs.sendMessage({
+      QueueUrl: queueUrl,
+      MessageBody: messageBody,
+    }).promise();
+
+  } catch (e) {
+    console.error(e);
+  }
+
   return {
     statusCode: 200,
     headers: {
