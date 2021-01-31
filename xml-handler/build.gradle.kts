1c9582e212d6d5b0f2ae2a84fc899b6fe65a9920
t-kurihara <takuya.kurihara00@gmail.com>
2021-01-31 23:50:59 +0900

    Unify common dependencies.

--- xml-handler/build.gradle.kts
+++ xml-handler/build.gradle.kts
@@ -2,15 +2,26 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.21"
-    application
 }
 
 allprojects {
+    apply(plugin = "kotlin")
+
     group = "me.user"
     version = "1.0-SNAPSHOT"
 
     repositories {
         mavenCentral()
+    }
+
+    dependencies {
+        implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+        implementation("joda-time:joda-time:2+")
+        implementation("org.slf4j:slf4j-api:1+")
+        implementation("ch.qos.logback:logback-classic:1+")
+        testImplementation(kotlin("test-junit5"))
+        testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
+        testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
     }
 }
 
@@ -20,24 +31,23 @@ subprojects {
     }
 }
 
-dependencies {
-    subprojects.forEach {
-        implementation(it)
+project(":s3-connector") {
+    dependencies {
+        implementation(project(":entities"))
+        implementation(project(":csv-converter"))
+        implementation(project(":dynamo-db-connector"))
+        implementation("software.amazon.awssdk:bom:2.+")
+        implementation("software.amazon.awssdk:s3:2.+")
+        implementation("software.amazon.awssdk:ssm:2.+")
     }
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("software.amazon.awssdk:bom:2.+")
-    implementation("software.amazon.awssdk:s3:2.+")
-    implementation("software.amazon.awssdk:ssm:2.+")
-    implementation("com.google.code.gson:gson:2.8.6")
-    implementation("com.opencsv:opencsv:5+")
-    implementation("joda-time:joda-time:2+")
-    implementation("org.slf4j:slf4j-api:1+")
-    implementation("ch.qos.logback:logback-classic:1+")
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
+project(":csv-converter") {
+    dependencies {
+        implementation(project(":entities"))
+        implementation("com.opencsv:opencsv:5+")
+    }
+}
 
 tasks.test {
     useJUnitPlatform()
--- xml-handler/xml-to-csv-converter/build.gradle.kts
+++ xml-handler/xml-to-csv-converter/build.gradle.kts
@@ -10,12 +10,6 @@ application {
 dependencies {
     implementation(project(":csv-converter"))
     implementation(project(":s3-connector"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
-    implementation("org.slf4j:slf4j-api:1+")
-    implementation("ch.qos.logback:logback-classic:1+")
-    testImplementation(kotlin("test-junit5"))
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.6.0")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.6.0")
 }
 
 tasks {
